2549771aebb1ca768a34972bf21c2cbdfd8b765b
ko-shiro-hap <koushirou.iino@icloud.com>
2023-02-08 21:25:51 +0900

    add: Homeの見た目作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,21 +2,26 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="flex px-10 py-8 w-screen justify-between items-end text-primary font-bold">
-      <h1 className="text-3xl">Koshiro’s_Portfolio</h1>
-      <nav>
-        <ul className="flex gap-5 text-xl">
+    <header className="flex px-4 md:px-10 py-8 w-screen justify-between items-end text-primary">
+      <Link href="/">
+        <h1 className="text-3xl font-bold">Koshiro’s_Portfolio</h1>
+      </Link>
+      <div className="relative md:hidden w-10 h-10 border border-primary rounded-full cursor-pointer transition-all hover:scale-110">
+        <div className="absolute top-1/2 left-1/2 w-3 h-3 border border-primary rounded-full transform transition-all -translate-x-1/2 -translate-y-1/2 hover:bg-primary"></div>
+      </div>
+      <nav className="hidden md:block">
+        <ul className="flex gap-5 text-xl font-medium">
           <li>
-            <Link href="/">Profile</Link>
+            <Link href="/profile" className="hover:underline">Profile</Link>
           </li>
           <li>
-            <Link href="/">Experience</Link>
+            <Link href="/experience" className="hover:underline">Experience</Link>
           </li>
           <li>
-            <Link href="/">Portfolio</Link>
+            <Link href="/portfolio" className="hover:underline">Portfolio</Link>
           </li>
           <li>
-            <Link href="/">Contact</Link>
+            <Link href="/contact" className="hover:underline">Contact</Link>
           </li>
         </ul>
       </nav>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,27 @@ import Link from "next/link";
 export default function Home({ formattedDate } : { formattedDate: string }) {
   return (
     <>
-      <section className="w-full">
-        <nav>
-          <Link href="">Profile</Link>
-          <Link href="">Experience</Link>
-          <Link href="">Portfolio</Link>
-          <Link href="">Contact</Link>
-        </nav>
+      <section className="relative w-full">
+        <div className="hidden md:block absolute left-10">
+          <nav className="relative w-48 h-48 lg:w-64 lg:h-64 xl:w-80 xl:h-80 text-3xl lg:text-4xl font-bold border border-primary rounded-full transition-all hover:scale-110">
+            <div className="absolute top-1/2 left-1/2 w-12 h-12 lg:w-20 lg:h-20 xl:w-28 xl:h-28 border border-primary rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
+            <div className="absolute top-1/6 -left-10 py-2 bg-white rounded-full">
+              <Link href="/profile" className="text-stroke transition-all hover:text-primary hover:text-4xl lg:hover:text-5xl">Profile</Link>
+            </div>
+            <div className="absolute top-1/6 left-36 lg:left-48 xl:left-62 py-2 bg-white rounded-full">
+              <Link href="/experience" className="text-stroke transition-all hover:text-primary hover:text-4xl lg:hover:text-5xl">Experience</Link>
+            </div>
+            <div className="absolute bottom-1/6 -left-10 py-2 bg-white rounded-full">
+              <Link href="portfolio" className="text-stroke transition-all hover:text-primary hover:text-4xl lg:hover:text-5xl">Portfolio</Link>
+            </div>
+            <div className="absolute bottom-1/6 left-36 lg:left-48 xl:left-62 py-2 bg-white rounded-full">
+              <Link href="contact" className="text-stroke transition-all hover:text-primary hover:text-4xl lg:hover:text-5xl">Contact</Link>
+            </div>
+          </nav>
+        </div>
         <div className="text-right text-secondary font-bold">
           <h2 className="pb-10 text-8xl xl:text-9xl">HELLO<br/>WORLD</h2>
-          <p className="text-2xl sm:text-3xl md:text-4xl md:leading-relaxed">My name is <span className="text-4xl md:text-5xl text-stroke">Koshiro Iino</span>.
+          <p className="text-2xl md:text-3xl xl:text-4xl md:leading-relaxed">My name is <span className="text-4xl md:text-5xl text-stroke">Koshiro Iino</span>.
           <br />
           I offer the best value in my engineering.</p>
         </div>
