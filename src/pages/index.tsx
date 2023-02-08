a89d46ae7b49835fd58f4139f0852424ea4525a5
ko-shiro-hap <koushirou.iino@icloud.com>
2023-02-09 00:21:35 +0900

    fixed: 不要な部分の修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
-import Content from "@/components/Content";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
 import VisualNav from "@/components/VisualNav";
-import Link from "next/link";
 
-export default function Home({ formattedDate } : { formattedDate: string }) {
+// export default function Home({ formattedDate } : { formattedDate: string }) {
+export default function Home() {
   return (
     <>
       <section className="relative w-full">
@@ -23,12 +19,12 @@ export default function Home({ formattedDate } : { formattedDate: string }) {
   );
 }
 
-export async function getStaticProps() {
-  const buildDate = Date.now();
-  const formattedDate = new Intl.DateTimeFormat("en-US", {
-    dateStyle: "long",
-    timeStyle: "long",
-  }).format(buildDate);
+// export async function getStaticProps() {
+//   const buildDate = Date.now();
+//   const formattedDate = new Intl.DateTimeFormat("en-US", {
+//     dateStyle: "long",
+//     timeStyle: "long",
+//   }).format(buildDate);
 
-  return { props: { formattedDate } };
-}
+//   return { props: { formattedDate } };
+// }
--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,10 +1,4 @@
-import Content from "@/components/Content";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
-import Sidebar from "@/components/Sidebar";
-import VisualNav from "@/components/VisualNav";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Profile() {
   return (
@@ -40,13 +34,3 @@ export default function Profile() {
     </>
   );
 }
-
-// export async function getStaticProps() {
-//   const buildDate = Date.now();
-//   const formattedDate = new Intl.DateTimeFormat("en-US", {
-//     dateStyle: "long",
-//     timeStyle: "long",
-//   }).format(buildDate);
-
-//   return { props: { formattedDate } };
-// }
