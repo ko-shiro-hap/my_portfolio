0795b2cb7bba5441983d0d7a82f2e2089b33afce
ko-shiro-hap <koushirou.iino@icloud.com>
2023-02-09 01:31:30 +0900

    add: Portfolioページ作成

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,36 +1,49 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Portfolio() {
   return (
     <>
-      <section className="mx-auto px-4 pt-10 lg:pt-0 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg max-h-full w-full text-secondary">
+      <section className="mx-auto px-4 pt-10 lg:pt-0 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg max-h-full w-full text-primary">
         <h2 className="text-4xl font-bold">Portfolio</h2>
         <span className="text-lg font-bold">制作物</span>
+        {/* 制作物が増えたら繰り返す */}
         <div className="mx-auto px-4 pt-10 max-w-screen-sm xl:max-w-screen-md w-full">
           <div>
-            <div className="relative w-full h-48 mx:h-64 sm:h-72 md:h-80 lg:h-96 xl:h-105 border border-primary">
-              <Image src="/images/portfolio_site.png" fill alt="portfolio" className="w-full object-contain rounded"/>
+            <div className="relative w-full h-48 mx:h-64 sm:h-72 md:h-80 lg:h-96 xl:h-105 border border-primary transition-all hover:opacity-50">
+              <Link href="/">
+                <Image src="/images/portfolio_site.png" fill alt="portfolio" className="w-full object-contain rounded"/>
+              </Link>
             </div>
-              <h3>Koshiro’s_Portfolio</h3>
+            <div className="md:flex gap-1 pt-3 justify-between items-start font-bold">
+              <h3 className="text-xl lg:text-2xl xl:text-3xl">Koshiro’s_Portfolio</h3>
               <div>
-                <h4>CATEGORY</h4>
-                <ul>
-                  <li>Front-end</li>
-                  <li>Design</li>
+                <h4 className="lg:text-xl">CATEGORY</h4>
+                <ul className="flex gap-1 text-sm text-white flex-wrap">
+                  <li className="px-1 bg-primary">Front-end</li>
+                  <li className="px-1 bg-primary">Design</li>
                 </ul>
               </div>
               <div>
-                <h4>TAG</h4>
-                <ul>
-                  <li>Next.js</li>
-                  <li>TypeScript</li>
-                  <li>Tailwindcss</li>
-                  <li>AWS</li>
+                <h4 className="lg:text-xl">TAG</h4>
+                <ul className="flex gap-1 text-sm flex-wrap">
+                  <li className="px-1 bg-white border border-primary">Next.js</li>
+                  <li className="px-1 bg-white border border-primary">TypeScript</li>
+                  <li className="px-1 bg-white border border-primary">Tailwindcss</li>
+                  <li className="px-1 bg-white border border-primary">AWS</li>
                 </ul>
               </div>
-              <button>In More Detail</button>
+            </div>
+              <Link href="https://qiita.com/ko-shiro-hap/items/0ee1e929128ea39694af">
+                <div className="inline-flex py-1 items-end transition-all hover:underline">
+                  <div className="mr-0.5 w-0.5 h-5 bg-primary"></div>
+                  <div className="mr-1 w-0.25 h-4 bg-primary"></div>
+                  <p className="lg:text-xl font-bold leading-none">In More Detail</p>
+                </div>
+              </Link>
           </div>
         </div>
+        {/* ここまで繰り返す */}
       </section>
     </>
   );
