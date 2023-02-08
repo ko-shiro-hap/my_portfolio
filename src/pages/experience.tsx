be967bbb093bb2b0b9a952693dd9df87b7005683
ko-shiro-hap <koushirou.iino@icloud.com>
2023-02-09 01:35:58 +0900

    add: 制作予定のファイルを作成

---
+++ src/pages/experience.tsx
@@ -0,0 +1,11 @@
+export default function Experience() {
+  return (
+    <>
+      <section className="mx-auto px-4 pt-10 lg:pt-0 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg max-h-full w-full text-secondary">
+        <h2 className="text-4xl font-bold">Experience</h2>
+        <span className="text-lg font-bold">今までの経験</span>
+        <div>作成中です</div>
+      </section>
+    </>
+  );
+}
