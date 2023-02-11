42e07248d9cdd69f1e45a271ade31f5b9b1ea7ee
ko-shiro-hap <koushirou.iino@icloud.com>
2023-02-11 11:00:15 +0900

    add: Contactのhtmlを作成

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,16 @@
 export default function Contact() {
   return (
     <>
-      <section className="mx-auto px-4 pt-10 lg:pt-0 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg max-h-full w-full text-secondary">
+      <section className="mx-auto px-4 pt-10 lg:pt-0 max-w-screen-sm lg:max-w-screen-md xl:max-w-screen-lg max-h-full w-full text-secondary text-center">
         <h2 className="text-4xl font-bold">Contact</h2>
         <span className="text-lg font-bold">お問い合わせ</span>
-        <div>作成中です</div>
+        <div>
+          <p>以下からお問い合わせください！</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M246.6 470.6c-12.5 12.5-32.8 12.5-45.3 0l-160-160c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 402.7 361.4 265.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3l-160 160zm160-352l-160 160c-12.5 12.5-32.8 12.5-45.3 0l-160-160c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L224 210.7 361.4 73.4c12.5-12.5 32.8-12.5 45.3 0s12.5 32.8 0 45.3z"/></svg>
+          {/* Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. */}
+        </div>
+        <button>Contact Me！</button>
+        <p>SNSからの連絡もお待ちしております</p>
       </section>
     </>
   );
